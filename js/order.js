8845bf6a35ff0aff99216b69e599354f0541ca3d
ian <shemian092@gmail.com>
2020-03-03 14:52:16 +0300

    add oder files

--- js/order.js
+++ js/order.js
@@ -6,7 +6,8 @@ function quantityChange() {
     var prize_crust_size = parseInt(document.getElementById("crust").value)
     var quantity = parseInt(document.getElementById("number").value);  
     var total_prize= (prize_size + prize_topping + prize_crust_size) * quantity;
-    alert( "Thank you for choosing on us. You pizza delivery will be done soon, kindly send us your location.You total prize is " + total_prize + " Because we do a free delivery!")
+    var where = prompt("Enter your location within Nairobi")
+    alert( "Thank you for choosing on us. You pizza delivery will be done soon,  we have recived your location.You total prize is " + total_prize + " Because we do a free delivery! Your oder will be delivered in " + where + " thank you")
    
     
 }
